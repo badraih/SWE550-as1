605bbcb8c85d223826e7f5e99cdab18efea2c11c
Badriah Alshehri <badraihalshehri.cs@gmail.com>
2021-10-30 20:24:47 +0300

    Update Indext.php

--- src/index.php
+++ src/index.php
@@ -37,5 +37,12 @@ $result -> free_result();
 $conn->close();
 ?>
 </div>
+
+<form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
 </body>
 </html>
